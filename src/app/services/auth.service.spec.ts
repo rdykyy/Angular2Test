bfaa77a804d8c51e90e07430fee5aab5dd94e69c
Rdykyy <rdykyy@gmail.com>
2017-02-06 12:37:39 +0200

    * Added AuthService Unit tests

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,16 +1,31 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { AuthServiceService } from './auth-service.service';
+import { AuthService } from './auth.service';
+import { CookieService } from 'angular2-cookie/services/cookies.service';
+
 
 describe('AuthServiceService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthServiceService]
+      providers: [AuthService, CookieService]
     });
   });
 
-  it('should ...', inject([AuthServiceService], (service: AuthServiceService) => {
+  it('should ...', inject([AuthService], (service: AuthService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should set logged in to true and cookie to 1', inject([AuthService, CookieService], (authService: AuthService, cookieService: CookieService) => {
+    authService.login();
+    expect(authService.isLoggedIn()).toBe(true);
+    expect(cookieService.get('loggedIn')).toBe('1');
+  }));
+
+  it('should set logged in to false and and remove cookie', inject([AuthService, CookieService], (authService: AuthService, cookieService: CookieService) => {
+    authService.login();
+    authService.logout();
+    expect(authService.isLoggedIn()).toBe(false);
+    expect(cookieService.get('loggedIn')).toBe(undefined);
   }));
 });
